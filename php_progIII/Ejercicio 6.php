bc26d35bc6dd0f75bed841678d41c1d65a1c1fe5
FabianRolon <fabi18fr@gmail.com>
2020-08-28 14:31:16 -0300

    Ejercicio 6 terminado

--- php_progIII/Ejercicio 6.php
+++ php_progIII/Ejercicio 6.php
@@ -1,7 +1,27 @@
 <?php
 
+for ($i=0; $i < 6; $i++) { 
+    $vec[$i] = rand(1,10);
+}
+$contador = 0;
+$suma = 0;
+$promedio;
 
+foreach ($vec as $value) {
+    $suma += $value;
+    $contador++;
+}
 
+$promedio = $suma/$contador;
+echo $promedio;
+echo "<br>";
+if($promedio < 6){
+    echo "El promedio es menor a 6";
+}elseif($promedio == 6){
+    echo "El promedio es igual a 6";
+}else{
+    echo "El promedio es mayor a 6";
+}
 
 
 
