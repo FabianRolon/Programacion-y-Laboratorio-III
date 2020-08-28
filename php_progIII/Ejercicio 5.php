20ed6a2a63a97558b42dd3a53ff23cca1ebdc7f8
FabianRolon <fabi18fr@gmail.com>
2020-08-28 07:12:47 -0300

    agrego html a ej 5

--- php_progIII/Ejercicio 5.php
+++ php_progIII/Ejercicio 5.php
@@ -1,9 +1,9 @@
 <?php
 
 $conversion = new NumberFormatter("es-ES", NumberFormatter::SPELLOUT);
-$n = 1055455;
-$izquierda = intval(floor($n));
-$derecha = intval(($n - floor($n)) * 100);
+$numero = $_POST['numero'];
+$izquierda = intval(floor($numero));
+$derecha = intval(($numero - floor($numero)) * 100);
 echo $conversion->format($izquierda) . " coma " . $conversion->format($derecha);
 
 
